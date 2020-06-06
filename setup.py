d7962c6433b8b20ea68b87ebd65ae737034e74e9
ShashankBice <sbaglapl@uw.edu>
2020-06-06 00:07:27 -0700

    initial setup.py file

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='skysat_stereo',
+      version='0.1',
+      description='library for DEM generation workflows from Planet SkySat-C imagery ',
+      author='Shashank Bhushan and Team 3D',
+      author_email='sbaglapl@uw.edu',
+      url='https://github.com/uw-cryo/skysat_stereo.git',
+      packages=['skysat_stereo.py'],
+      install_requires=['requests']
+      )
