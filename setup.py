efbe6cbe65f2036f510c18e510eb2a98b994cdf8
ShashankBice <sbaglapl@uw.edu>
2020-06-28 20:58:32 -0700

    final port, clean setup

--- setup.py
+++ setup.py
@@ -8,6 +8,6 @@ setup(name='skysat_stereo',
       author='Shashank Bhushan and Team 3D',
       author_email='sbaglapl@uw.edu',
       url='https://github.com/uw-cryo/skysat_stereo.git',
-      packages=['skysat_stereo.py'],
+      packages=['skysat_stereo'],
       install_requires=['requests']
       )
