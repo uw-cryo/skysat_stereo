12836a54fc940462d8c7c125de4f97fe0629b841
ShashankBice <sbaglapl@uw.edu>
2020-09-29 11:04:26 -0700

    add option of orthorectification

--- scripts/skysat_orthorectify.py
+++ scripts/skysat_orthorectify.py
@@ -29,7 +29,8 @@ def get_parser():
 	parser.add_argument('-frame_index',default='None',help="frame index to read frame's actual Ground sampling distance",required=False)
 	orthomosaic_choice = [1,0]
 	parser.add_argument('-orthomosaic',default=0,type=int,choices=orthomosaic_choice, help="if mode is science, enabling this (1) will also produce a final orthomosaic (default: %(default)s)")
-	data_choices = ['video','triplet']
+	parser.add_argument('-copy_rpc',default=0,type=int,choices=orthomosaic_choice,help='if mode is science, enabling this (1) will copy rpc metadata in the orthoimage (default: %(default)s)')
+    data_choices = ['video','triplet']
 	parser.add_argument('-data',default='triplet',choices=data_choices,help="select if mosaicing video or triplet product in science mode (default: %(default)s)")
 	return parser
 
@@ -137,6 +138,9 @@ def main():
         with open(ortho_log,'w') as f:
             for log in ortho_logs:
                 f.write(log)
+        if args.copy_rpc == 1:
+            print("Copying RPC from native image to orthoimage in parallel")
+            copy_rpc_out = p_map(skysat.copy_rpc,img_list,out_list,num_cpus=cpu_count())
         if args.orthomosaic == 1:
             print("Will also produce median, weighted average and highest resolution orthomosaic")
             if args.data == 'triplet':
