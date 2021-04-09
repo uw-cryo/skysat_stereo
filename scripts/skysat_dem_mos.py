1c1ce2d6ade02adca012b07718abb1689170f1eb
ShashankBice <sbaglapl@uw.edu>
2021-04-08 22:34:58 -0700

    adjust mosaicking for cross_track DEMs

--- scripts/skysat_dem_mos.py
+++ scripts/skysat_dem_mos.py
@@ -5,6 +5,7 @@ import numpy as np
 import argparse
 from skysat_stereo import asp_utils as asp
 from skysat_stereo import skysat
+from tqdm import tqdm
 from p_tqdm import p_map
 import itertools
 from pygeotools.lib import iolib,warplib
@@ -16,6 +17,7 @@ def getparser():
     parser.add_argument('-identifier',help='if we want to mosaic individually aligned DEM which have been produced by skysat_coreg.py, place the identifiers here',required=False,default=None)
     mode_ch = ['video','triplet']
     parser.add_argument('-mode',default='triplet',choices=mode_ch,help="select if mosaicing video or triplet stereo output DEMs (default: %(default)s)")
+    parser.add_argument('-tile_size',default=None,help='Tile size for tiled processing, helpful on nodes with less memory or if num_dems are large')
     binary_ch = [1,0]
     parser.add_argument('-filter_dem',choices=binary_ch,default=1,type=int,
         help="filter video DEM composites using max NMAD and min count combination (default: %(default)s)")
@@ -42,47 +44,77 @@ def main():
         identifier = ''
     if args.mode == 'triplet':
         dir_list = sorted(glob.glob(os.path.join(dir,'20*/')))
-        valid_for_nadir_dir = []
-        valid_for_aft_dir = []
-        valid_nadir_aft_dir = []
-        for for_nadir_dir in sorted(glob.glob(os.path.join(dir_list[0],'*/'))):
+        print(f"Number of combinations {len(dir_list)}")
+        
+        def valid_disp(direc):
             try:
-               D_sub = iolib.fn_getma(os.path.join(for_nadir_dir,'run-D_sub.tif'),3)
-               stats = [np.percentile(D_sub.compressed(),(2,98)),np.mean(D_sub.compressed())]
-               DEM = glob.glob(os.path.join(for_nadir_dir,'run*{}*-DEM.tif'.format(identifier)))[0]
-               valid_for_nadir_dir.append(for_nadir_dir)
+                D_sub = iolib.fn_getma(os.path.join(direc,'run-D_sub.tif'),3)
+                stats = np.percentile(D_sub.compressed(),(2,98))
+                direc_out = glob.glob(os.path.join(direc,'run*{}*-DEM.tif'.format(identifier)))[0]
+                out = (direc_out,True)
             except:
-               continue
-        for for_aft_dir in sorted(glob.glob(os.path.join(dir_list[1],'*/'))):
-            try:
-               # see ASP issue for this dirty hack: https://github.com/NeoGeographyToolkit/StereoPipeline/issues/308
-                D_sub = iolib.fn_getma(os.path.join(for_aft_dir,'run-D_sub.tif'),3)
-                stats = [np.percentile(D_sub.compressed(),(2,98)),np.mean(D_sub.compressed())]
-                DEM = glob.glob(os.path.join(for_aft_dir,'run*{}*-DEM.tif'.format(identifier)))[0]
-                valid_for_aft_dir.append(for_aft_dir)
-            except:
-                continue
-        for nadir_aft_dir in sorted(glob.glob(os.path.join(dir_list[2],'*/'))):
-            try:
-                D_sub = iolib.fn_getma(os.path.join(nadir_aft_dir,'run-D_sub.tif'),3)
-                stats = [np.percentile(D_sub.compressed(),(2,98)),np.mean(D_sub.compressed())]
-                DEM = glob.glob(os.path.join(nadir_aft_dir,'run*{}*-DEM.tif'.format(identifier)))[0]
-                valid_nadir_aft_dir.append(nadir_aft_dir)
-            except:
-                continue
-        for_nadir_list = [glob.glob(os.path.join(dir,'run*{}*-DEM.tif'.format(identifier)))[0] for dir in valid_for_nadir_dir]
-        nadir_aft_list = [glob.glob(os.path.join(dir,'run*{}*-DEM.tif'.format(identifier)))[0] for dir in valid_nadir_aft_dir]
-        for_aft_list = [glob.glob(os.path.join(dir,'run*{}*-DEM.tif'.format(identifier)))[0] for dir in valid_for_aft_dir]
-        total_dem_list = for_nadir_list+for_aft_list+nadir_aft_list
-        stats_list = ['nmad','count','median']
-        print('total dems are {}'.format(len(total_dem_list)))
-        out_fn_list = [os.path.join(out_folder,'triplet_{}_mos.tif'.format(stat)) for stat in stats_list]
-        print("Mosaicing output total per-pixel nmad, count, nmad and 3 DEMs from 3 stereo combinations in parallel")
-        dem_mos_log = p_map(asp.dem_mosaic,[total_dem_list]*3+[for_aft_list,nadir_aft_list,for_nadir_list],out_fn_list+[os.path.join(out_folder,x) for x in ['for_aft_dem_median_mos.tif', 'nadir_aft_dem_median_mos.tif', 'for_nadir_dem_median_mos.tif']],['None']*6,[None]*6,stats_list+['median']*3,[None]*6,num_cpus=4)
+                out = (0,False)
+            return out
+        # find all valid DEMs
+        total_cpu = iolib.cpu_count()
+        n_proc = total_cpu - np.arange(len(dir_list))
+        #this setup is so that old pools are discarded and new ones with new number of workers are created
+        valid_dem_dir_list = []
+        for idx,direc in enumerate(tqdm(dir_list)):
+            comb_dir_list = sorted(glob.glob(os.path.join(direc,'*/')))
+            results = p_map(valid_disp,comb_dir_list,num_cpus=n_proc[idx])
+            t_val = [r[1] for r in results]
+            valid_dirs = list(itertools.compress([r[0] for r in results],t_val))
+            valid_dem_dir_list.append(valid_dirs)
+        
+
+        # naming logic for pairwise and triplet/multiview composites
+        mdt1,t1,mdt2,t2 = [[],[],[],[]]
+        combination_out_list = []
+        if len(dir_list)>3:
+            print("Input is cross-track")
+        if dir_list[0][-1] == '/':
+            dir_list = [x[:-1] for x in dir_list]
+        for direc in dir_list:
+
+            combination_out_list.append(os.path.join(out_folder,os.path.basename(direc)+'_wt_avg_mos.tif'))
+            a,b,c,d = os.path.basename(direc).split('_')
+            mdt1.append(a) #master date (year month date)
+            t1.append(b) # time of day in seconds
+            mdt2.append(c)
+            t2.append(d)
+        if len(direc)>3:
+            composite_prefix = 'multiview_'+'_'.join(np.unique(mdt1+mdt2))+'__'+'_'.join(np.unique(t1+t2))
+        else:
+            composite_prefix = 'triplet_'+'_'.join(np.unique(mdt1+mdt2))+'__'+'_'.join(np.unique(t1+t2))
+
+        # produce bistereo pairwise mosaics
+        len_combinations = len(combination_out_list)
+        tile_size = args.tile_size
+
+        if len_combinations > 3:
+            # force tiled processing in case of multiview mosaicking
+            if not tile_size:
+                tile_size = 400
+       
+        mos_log = p_map(asp.dem_mosaic,valid_dem_dir_list,combination_out_list,
+                ['None']*len_combinations,[None]*len_combinations,
+                [None]*len_combinations,[tile_size]*len_combinations)
+
+        if len_combinations > 2:
+            print("Producing triplet/multiview composites")
+            total_dem_list = list(itertools.chain.from_iterable(valid_dem_dir_list))
+            print(f"Mosaicing {len(total_dem_list)} DEM strips using median, wt_avg, count, nmad operators")
+            stats_list = [None,'median','nmad','count']
+            out_fn_list = [os.path.join(out_folder,
+                            '{}_{}_mos.tif'.format(composite_prefix,stat)) for stat in ['wt_avg','median','nmad','count']]
+            composite_mos_log = p_map(asp.dem_mosaic,[total_dem_list]*4,out_fn_list,['None']*4,[None]*4,stats_list,
+                                [tile_size]*4,num_cpus=4)
+
         out_log_fn = os.path.join(out_folder,'skysat_triplet_dem_mos.log')
         print("Saving triplet DEM mosaic log at {}".format(out_log_fn))
         with open(out_log_fn,'w') as f:
-            for log in dem_mos_log:
+            for log in mos_log+composite_mos_log:
                 f.write(log) 
     elif args.mode=='video':
         dir_list = sorted(glob.glob(os.path.join(dir,'1*/')))
