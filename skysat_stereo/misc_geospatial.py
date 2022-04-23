11e0d3ebc8ea486166370d50f29877ba01851e99
ShashankBice <sbaglapl@uw.edu>
2022-04-23 16:45:54 -0700

    ndvtrim_f

--- skysat_stereo/misc_geospatial.py
+++ skysat_stereo/misc_geospatial.py
@@ -71,4 +71,39 @@ def clip_raster_by_shp_disk(r_fn,shp_fn,invert=False):
     out_fn = os.path.splitext(r_fn)[0]+'_shpclip.tif'
     iolib.writeGTiff(r, out_fn, r_ds)
 
+def ndvtrim_function(src_fn):
+    """ 
+    # this is a direct port from https://github.com/dshean/pygeotools/blob/master/pygeotools/trim_ndv.py
+    # intended to make it a function
+    """
+    src_fn = args.src_fn
+    if not iolib.fn_check(src_fn):
+        sys.exit("Unable to find src_fn: %s" % src_fn)
+    
+    #This is a wrapper around gdal.Open()
+    src_ds = iolib.fn_getds(src_fn)
+    src_gt = src_ds.GetGeoTransform()
+
+    print("Loading input raster into masked array")
+    bma = iolib.ds_getma(src_ds)
+
+    print("Computing min/max indices for mask")
+    edge_env = malib.edgefind2(bma, intround=True)
+
+    print("Updating output geotransform")
+    out_gt = list(src_gt)
+    #This should be OK, as edge_env values are integer multiples, and the initial gt values are upper left pixel corner
+    #Update UL_X
+    out_gt[0] = src_gt[0] + src_gt[1]*edge_env[2]
+    #Update UL_Y, note src_gt[5] is negative
+    out_gt[3] = src_gt[3] + src_gt[5]*edge_env[0]
+    out_gt = tuple(out_gt)
+
+
+    out_fn = os.path.splitext(src_fn)[0]+'_trim.tif'
+    print("Writing out: %s" % out_fn)
+    #Extract valid subsection from input array
+    #indices+1 are necessary to include valid row/col on right and bottom edges
+    iolib.writeGTiff(bma[edge_env[0]:edge_env[1]+1, edge_env[2]:edge_env[3]+1], out_fn, src_ds, gt=out_gt)
+    bma = None
 
